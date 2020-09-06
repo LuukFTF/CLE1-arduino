a5c95fd7dbab12c7fe3e8faa48046196094fc510
LuukFTF <lucasvdvegt@gmail.com>
2020-09-06 23:40:25 +0200

    added motor code

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,24 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int pos = 0;
+int motorPin = 3;
+
 void setup() {
+  myservo.attach(9);
 
-
+  digitalWrite(motorPin, HIGH);
 }
 
 void loop() {
+  moveTo(1);
+  delay(1000);
+  moveTo(360);
+  delay(1000);  
 
+}
 
+int moveTo(int pos) {
+  myservo.write(pos);
 }
